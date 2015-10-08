31a9775a7c9b9c88ba31805f173ad7345836c496
mickelange <mcreusot@gmail.com>
2015-10-08 11:12:43 +0200

    creation du projet

---
+++ Formation/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation
+{
+    class Book
+    {
+        public string Isbn
+        {
+            get; set;
+        }
+        public int Id
+        {
+            get; set;
+        }
+        public DateTime Date
+        {
+            get; set;
+        }
+        public string Titre
+        {
+            get; set;
+        }
+        public int NbPages
+        {
+            get; set;
+        }
+        public string Lang
+        {
+            get; set;
+        }
+
+    }
+}
---
+++ Formation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.Titre = "java";
+            Console.WriteLine(b.Titre);
+            Console.ReadKey();
+
+
+        }
+    }
+}
